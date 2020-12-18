90314a0522b921ed35037b9c9a966295df20020d
Loreina Salalila <vialoraine.gg@gmail.com>
2020-12-18 12:54:16 -0800

    path error on heroku

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // routes
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
 // creating connection to mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
